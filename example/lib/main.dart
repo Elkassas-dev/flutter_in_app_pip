db949bb0f6ad94413ee258784126a42c3cc7eb63
Ahmed Elkassas <elkassasdev@gmail.com>
2026-09-12 01:10:27 +0300

    Update analysis options, dependencies, and imports to use material_ui package

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:flutter_in_app_pip/flutter_in_app_pip.dart';
 
 class MyHttpOverrides extends HttpOverrides {
